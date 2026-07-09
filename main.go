46994487d63a0b8f0523644ffc109b208ad14d8c
Fregres <dsergv23@gmail.com>
2026-07-09 22:09:04 +0300

    fix: configure HTTP server timeouts

--- main.go
+++ main.go
@@ -22,7 +22,14 @@ func main() {
 
 	srv := &Server{token: token}
 	const address = "127.0.0.1:6767"
-	server := &http.Server{Addr: address, Handler: srv.routes()}
+	server := &http.Server{
+		Addr:              address,
+		Handler:           srv.routes(),
+		ReadHeaderTimeout: 5 * time.Second,
+		ReadTimeout:       10 * time.Second,
+		WriteTimeout:      10 * time.Second,
+		IdleTimeout:       60 * time.Second,
+	}
 
 	log.Printf("server started on %s", address)
 	errChan := make(chan error, 1)
