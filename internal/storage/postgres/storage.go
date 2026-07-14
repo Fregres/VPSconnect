b647b7e5b6bb1768af36d471d150f653da326914
Fregres <dsergv23@gmail.com>
2026-07-14 21:35:02 +0300

    feat(storage): add PostgreSQL connection

---
+++ internal/storage/postgres/storage.go
@@ -0,0 +1,30 @@
+package postgres
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+type Storage struct {
+	pool *pgxpool.Pool
+}
+
+func New(ctx context.Context, databaseURL string) (*Storage, error) {
+	pool, err := pgxpool.New(ctx, databaseURL)
+	if err != nil {
+		return nil, fmt.Errorf("create PostgreSQL pool: %w", err)
+	}
+
+	if err := pool.Ping(ctx); err != nil {
+		pool.Close()
+		return nil, fmt.Errorf("ping PostgreSQl: %w", err)
+	}
+
+	return &Storage{pool: pool}, nil
+}
+
+func (s *Storage) Close() {
+	s.pool.Close()
+}
